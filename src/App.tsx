77304e8e912e621f26c727cf952469b1892ee946
clkbug <mail@clkbug.net>
2023-02-21 20:30:59 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+import Comp from './Comp';
+
+const App: Component = () => {
+  return (
+    <>
+      <h1>Task Tracker 2</h1>
+      <Comp />
+    </>
+  );
+};
+
+export default App;
